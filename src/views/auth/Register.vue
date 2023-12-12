cc11bd3b28d42807d040483c6155a0cda224c487
Jose Miguel <jose.miguelq95@gmail.com>
2023-12-12 08:42:42 -0400

    plano

--- src/views/auth/Register.vue
+++ src/views/auth/Register.vue
@@ -8,21 +8,13 @@
           <div class="rounded-t mb-0 px-6 py-6">
             <div class="text-center mb-3">
               <h6 class="text-blueGray-500 text-sm font-bold">
-                Sign up with
+                Registrar com
               </h6>
             </div>
             <div class="btn-wrapper text-center">
               <button
-                class="bg-white active:bg-blueGray-50 text-blueGray-700 font-normal px-4 py-2 rounded outline-none focus:outline-none mr-2 mb-1 uppercase shadow hover:shadow-md inline-flex items-center font-bold text-xs ease-linear transition-all duration-150"
-                type="button"
-              >
-                <img alt="..." class="w-5 mr-1" :src="github" />
-                Github
-              </button>
-              <button
                 class="bg-white active:bg-blueGray-50 text-blueGray-700 font-normal px-4 py-2 rounded outline-none focus:outline-none mr-1 mb-1 uppercase shadow hover:shadow-md inline-flex items-center font-bold text-xs ease-linear transition-all duration-150"
-                type="button"
-              >
+                type="button">
                 <img alt="..." class="w-5 mr-1" :src="google" />
                 Google
               </button>
@@ -31,7 +23,7 @@
           </div>
           <div class="flex-auto px-4 lg:px-10 py-10 pt-0">
             <div class="text-blueGray-400 text-center mb-3 font-bold">
-              <small>Or sign up with credentials</small>
+              <small>Ou com email</small>
             </div>
             <form>
               <div class="relative w-full mb-3">
@@ -39,12 +31,12 @@
                   class="block uppercase text-blueGray-600 text-xs font-bold mb-2"
                   htmlFor="grid-password"
                 >
-                  Name
+                  Nome
                 </label>
                 <input
                   type="email"
                   class="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
-                  placeholder="Name"
+                  placeholder="Jhon Snow"
                 />
               </div>
 
@@ -67,7 +59,7 @@
                   class="block uppercase text-blueGray-600 text-xs font-bold mb-2"
                   htmlFor="grid-password"
                 >
-                  Password
+                  Senha
                 </label>
                 <input
                   type="password"
@@ -84,9 +76,9 @@
                     class="form-checkbox border-0 rounded text-blueGray-700 ml-1 w-5 h-5 ease-linear transition-all duration-150"
                   />
                   <span class="ml-2 text-sm font-semibold text-blueGray-600">
-                    I agree with the
+                    Eu concordo com as
                     <a href="javascript:void(0)" class="text-emerald-500">
-                      Privacy Policy
+                      Politicas de privacidade
                     </a>
                   </span>
                 </label>
@@ -97,7 +89,7 @@
                   class="bg-blueGray-800 text-white active:bg-blueGray-600 text-sm font-bold uppercase px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 w-full ease-linear transition-all duration-150"
                   type="button"
                 >
-                  Create Account
+                  Criar conta
                 </button>
               </div>
             </form>
