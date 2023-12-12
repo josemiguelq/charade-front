ece1426445a50dfa595147c61e75cc3e4ca2449a
Jose Miguel <jose.miguelq95@gmail.com>
2023-12-11 23:18:14 -0400

    almost

--- src/components/Cards/CardEvaluation.vue
+++ src/components/Cards/CardEvaluation.vue
@@ -5,61 +5,13 @@
       <div class="px-6">
         <div class="flex flex-wrap justify-center">
           <div class="w-full px-4 text-center mt-20">
-            <div class="flex justify-center py-4 lg:pt-4 pt-8">
-              <div class="mr-4 p-3 text-center">
-                <span
-                  class="text-xl font-bold block uppercase tracking-wide text-blueGray-600"
-                >
-                  1
-                </span>
-                <span class="text-sm text-blueGray-400">Idade</span>
-              </div>
-              <div class="mr-4 p-3 text-center">
-                <span
-                  class="text-xl font-bold block uppercase tracking-wide text-blueGray-600"
-                >
-                  2
-                </span>
-                <span class="text-sm text-blueGray-400">Avaliações</span>
-              </div>
-              <div class="lg:mr-4 p-3 text-center">
-                <span
-                  class="text-xl font-bold block uppercase tracking-wide text-blueGray-600"
-                >
-                  89
-                </span>
-                <span class="text-sm text-blueGray-400">Plano de intervenções</span>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="text-center mt-8">
-          <h3
-            class="text-xl font-semibold leading-normal mb-2 text-blueGray-700"
-          >
-            Jenna Stones
-          </h3>      
-        </div>
-        <div class="mt-4 py-10 border-t border-blueGray-200 text-center">
-          <div class="flex flex-wrap justify-center">
-            <div class="w-full lg:w-9/12 px-4">
-              <p class="mb-4 text-lg leading-relaxed text-blueGray-700">
-                Joao gosta de gatos, gosta de interagir por gestos.
-              </p>            
-            </div>
           </div>
         </div>
         <div class="mt-4 py-10 border-t border-blueGray-200 text-center">
           <div class="flex flex-wrap justify-center">
             <div class="w-full lg:w-6/12 px-4">
-              <CardAction title="Avaliar" iconName="fas fa-chart-pie"/>            
-            </div>
-            <div class="w-full lg:w-6/12 px-4">
-              <CardAction title="Plano de intervenção"/>
-            </div>
-            <div class="w-full lg:w-6/12 px-4">
-              <CardAction title="Registro diário" descripiron="Registre a evoluçao"/>            
-            </div>          
+              <CardAction title="VB-MAPP" iconName="fas fa-chart-pie"/>            
+            </div>                     
           </div>
         </div>
       </div>
